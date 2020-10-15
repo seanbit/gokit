a0e94685dd36d8c052962f08659aa7daefb69667
seanbit <sean.vip.contact@foxmail.com>
2020-10-15 17:24:07 +0800

    rego foundation error

--- foundation/error.go
+++ foundation/error.go
@@ -1,5 +1,13 @@
 package foundation
 
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+	"testing"
+)
+
 type Error interface {
 	error
 	Code() int
@@ -12,13 +20,6 @@ type cerr struct {
 	msg  	string
 }
 
-func (this *cerr) Error() string {
-	if this.error == nil {
-		return ""
-	}
-	return this.error.Error()
-}
-
 func (this *cerr) Code() int {
 	return this.code
 }
@@ -27,10 +28,46 @@ func (this *cerr) Msg() string {
 	return this.msg
 }
 
-func NewError(err error, code int, msg string) Error {
+func (this *cerr) Unwrap() error {
+	return this.error
+}
+
+func NewError(err error, code int, msg string) error {
+	if err == nil {
+		err = errors.New("")
+	}
+	return fmt.Errorf("code:%d, msg:%s, err:%s", code, msg, err.Error())
+}
+
+func ParseError(err error) (Error, bool) {
+	if err == nil {
+		return nil, false
+	}
+	errRg := regexp.MustCompile(`^code:(\d+), msg:([\s\S]+), err:([\s\S]+)`)
+	params := errRg.FindStringSubmatch(err.Error())
+	if params[0] != err.Error() {
+		return nil, false
+	}
+	if len(params) != 4 {
+		return nil, false
+	}
+	code, err := strconv.Atoi(params[1])
+	if err != nil {
+		return nil, false
+	}
 	return &cerr{
-		error: err,
+		error: errors.New(params[3]),
 		code:  code,
-		msg:  msg,
+		msg:   params[2],
+	}, true
+}
+
+func TestParseError(t *testing.T) {
+	err := NewError(errors.New("haha-a, asd@!()uw-  wow! 这是汉语哎，還有繁體字呢。 hey834w"), 101, "some wrong")
+	e, ok := ParseError(err)
+	if ok {
+		fmt.Printf("code:%d, msg:%s, err:%s", e.Code(), e.Msg(), e.Error())
+	} else {
+		fmt.Println("not Error")
 	}
 }
